75a5df665e97e0bf3b7b82a7498951fa7d3151e3
MadhuraSawant <84861665+Madhura-saw@users.noreply.github.com>
2023-09-19 12:04:38 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export const Connection =async (username,password) =>{
     // const url =   `mongodb+srv://${username}:${password}@cluster0.my5txlu.mongodb.net/test`;
     // const url =  `mongodb://localhost:27017/projectdb`;
-    const url = `mongodb+srv://madhuraIPP:<password>@cluster0.exjnoa4.mongodb.net/?retryWrites=true&w=majority`;
+    const url = `mongodb+srv://<username>:<password>@cluster0.exjnoa4.mongodb.net/?retryWrites=true&w=majority`;
     try{
        await mongoose.connect(url, {useNewUrlParser:true});
        console.log('database connected sucessfully');
